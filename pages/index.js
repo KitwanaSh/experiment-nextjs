4f2cb7e1daf8d2807a4cbad809a6b562e5a06ce7
KitwanaSh <rodriguekitweze@gmail.com>
2023-07-24 20:11:02 +0200

    Feat: Try to find a solution

--- pages/index.js
+++ pages/index.js
@@ -3,18 +3,20 @@ import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default async function Home() {
+async function Home() {
   const data = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`)
   const res = await data.json()
 
   return (
     <main>
       <h1 className='text-lg my-2'>Hello Next Js 13</h1>
-      {res.results.map((movie) => (
+      {res.map((movie) => (
         <div>
-          <h1 key={movie.id}>{movie.title}</h1>
+          <h1>{movie.title}</h1>
         </div>
       ))}
     </main>
   )
 }
+
+export default Home

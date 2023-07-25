d99c7fcb1648157ff04806a4c76d6ba030aced7b
KitwanaSh <rodriguekitweze@gmail.com>
2023-07-25 16:03:31 +0200

    Feat: Continue later

---
+++ pages/Movie.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link"
+
+export default function Movie() {
+    return (
+        <div>
+            <h1>Title</h1>
+            <h2>Release date</h2>
+            <Link href='./Something'>
+                <img src="" alt="" />
+            </Link>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,37 @@
-import Image from 'next/image'
+import Movie from './Movie'
 import { Inter } from 'next/font/google'
 
 
 const inter = Inter({ subsets: ['latin'] })
 
+async function getData() {
+  const res = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`)
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+ 
+  // Recommendation: handle errors
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
+ 
+  return res.json()
+}
+ 
 export default async function Home() {
-  let res = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`).json()
-  let data = await res.json()
+  const data = await getData()
   console.log(data)
+ 
   return (
+    <>
     <main>
-      <h1 className='text-lg my-2'>Hello Next Js 13</h1>
+      <h1>Welcome to Next js 13</h1>
+      {data.results.map((movie) => (
+        <div>
+          <h1 key={movie.id}>{movie.title}</h1>
+        </div>
+      ))}
     </main>
+    </>
   )
 }

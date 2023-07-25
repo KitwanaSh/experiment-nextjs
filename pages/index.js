d6dfb444fbc52411a6736c7941c3202b2caa3edb
KitwanaSh <rodriguekitweze@gmail.com>
2023-07-25 15:22:04 +0200

    Feat: Add the await

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default async function Home() {
-  const res = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`)
-  const data = await res.json();
-  console.log(data);
-
+  let res = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`).json()
+  let data = await res.json()
+  console.log(data)
   return (
     <main>
       <h1 className='text-lg my-2'>Hello Next Js 13</h1>

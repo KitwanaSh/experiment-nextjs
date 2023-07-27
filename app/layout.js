4e45b398bfa6c2875f76b5d76e408269ab327c86
KitwanaSh <rodriguekitweze@gmail.com>
2023-07-27 14:22:59 +0200

    fix: Update the layout

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className} className="mx-32">{children}</body>
+      <body className={inter.className} className="mx-32 my-12">{children}</body>
     </html>
   )
 }

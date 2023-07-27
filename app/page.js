fc0f51848efbd74855da681fba65763d1ee0830f
KitwanaSh <rodriguekitweze@gmail.com>
2023-07-27 14:33:32 +0200

    Fix: remove console

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import Movie from "./Movie"
 export default async function Home() {
     const data = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`)
     const res = await data.json()
-    console.log(res)
+
   return (
     <main>
       <div className="grid gap-16 grid-cols-fluid">
